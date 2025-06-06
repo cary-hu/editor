12fc783921181fc75be48d43053a5c911e6ad14c
cary.hu <cary.hu@developertools.com>
2025-06-06 15:06:35 +0800

    fix: can not run test

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const base = require('../../jest.base.config');
+const base = require('../jest.base.config');
 
 module.exports = {
   ...base,
--- scripts/pkg-script.js
+++ scripts/pkg-script.js
@@ -9,26 +9,12 @@ const options = commandLineArgs(optionDefinitions);
 
 const pkgMap = {
   editor: '@toast-ui/editor',
-  react: '@toast-ui/react-editor',
-  vue: '@toast-ui/vue-editor',
   toastmark: '@toast-ui/toastmark',
-  chart: '@toast-ui/editor-plugin-chart',
-  color: '@toast-ui/editor-plugin-color-syntax',
-  code: '@toast-ui/editor-plugin-code-syntax-highlight',
-  table: '@toast-ui/editor-plugin-table-merged-cell',
-  uml: '@toast-ui/editor-plugin-uml',
 };
 
 const pathMap = {
-  editor: 'apps/editor',
-  react: 'apps/react-editor',
-  vue: 'apps/vue-editor',
+  editor: 'app',
   toastmark: 'libs/toastmark',
-  chart: 'plugins/chart',
-  color: 'plugins/color-syntax',
-  code: 'plugins/code-syntax-highlight',
-  table: 'plugins/table-merged-cell',
-  uml: 'plugins/uml',
 };
 
 let script;
@@ -44,36 +30,31 @@ Object.keys(options).forEach((key) => {
 });
 
 if (!script) {
-  throw new Error(
-    `You should choose "lint", "test", "test:types", "serve", "serve:ie", "build" as the type of script`
-  );
-}
-
-if (script === 'doc' || script === 'doc:dev') {
-  pkg = pkgMap['editor'];
-  path = pathMap['editor'];
+  throw new Error(`You should choose "lint", "test", "test:types", "serve", "build" as the type of script`);
 }
 
 if (!pkg) {
-  throw new Error(
-    `You should choose "editor", "react", "vue", "toastmark", "chart", "color", "code", "uml", "table"
-    as the configuration of type
-    `
-  );
+  throw new Error(`You should choose "editor", "toastmark" as the configuration of type`);
 }
 
-if (script === 'test') {
-  spawn('jest', ['--watch', '--projects', path], {
-    stdio: 'inherit',
-  }).on('exit', (code) => {
-    exit(code);
-  });
-} else {
-  spawn('lerna', ['run', '--stream', '--scope', pkg, script], {
-    stdio: 'inherit',
-    // use user's default shell (bash) instead of cmd on Windows
-    shell: process.env.SHELL || 'bash.exe',
-  }).on('exit', (code) => {
-    exit(code);
-  });
+const spawnConfiguration = {
+  stdio: 'inherit',
+  shell: process.env.SHELL || 'bash.exe',
 }
+
+let childProcess;
+
+if (script === 'test') {
+  childProcess = spawn('jest', ['--watch', '--projects', path], spawnConfiguration);
+} else {
+  childProcess = spawn('lerna', ['run', '--stream', '--scope', pkg, script], spawnConfiguration);
+}
+
+childProcess
+  .on('exit', (code) => {
+    exit(code);
+  })
+  .on('error', (err) => {
+    console.error(`Error occurred: ${err.message}`);
+    exit(1);
+  });
